e553e5b704cfe47630b74acef55c5f8905844554
Noah Bresler <noahbresler2002@gmail.com>
2020-10-09 04:02:30 -0500

    Create VeloPIDTuner.java file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VeloPIDTuner.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class VeloPIDTuner extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
