540f18dc6afbd7ccc79f22b084d8e4573ead84d3
Noah Bresler <noahbresler2002@gmail.com>
2021-01-21 21:53:32 -0600

    Update VelocityPIDController

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VelocityPIDFController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VelocityPIDFController.java
@@ -1,26 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
-import android.util.Log;
-
 import com.acmerobotics.roadrunner.control.PIDCoefficients;
 import com.acmerobotics.roadrunner.control.PIDFController;
-import com.acmerobotics.roadrunner.util.NanoClock;
-import com.qualcomm.robotcore.util.ElapsedTime;
+import com.acmerobotics.roadrunner.kinematics.Kinematics;
 import com.qualcomm.robotcore.util.MovingStatistics;
 
 public class VelocityPIDFController {
-    private static final int ACCEL_SAMPLES = 10;
+    private static final int ACCEL_SAMPLES = 3;
     private static final double VELOCITY_EPSILON = 20 + 1e-6;
 
     private PIDFController controller;
 
     private MovingStatistics accelSamples;
 
-    private NanoClock clock;
+    private double lastPosition = Double.NaN;
+    private double lastVelocity = Double.NaN;
 
-    private double lastPosition;
-    private double lastVelocity;
-    private double lastUpdateTimestamp;
+    private double kV;
+    private double kA;
+    private double kStatic;
 
     private VelocityPIDFController() {}
 
@@ -37,18 +35,21 @@ public class VelocityPIDFController {
     }
 
     public VelocityPIDFController(PIDCoefficients pid, double kV, double kA, double kStatic) {
-        controller = new PIDFController(pid, kV, kA, kStatic);
+        controller = new PIDFController(pid);
         accelSamples = new MovingStatistics(ACCEL_SAMPLES);
-        clock = NanoClock.system();
+
+        this.kV = kV;
+        this.kA = kA;
+        this.kStatic = kStatic;
+
         reset();
     }
 
     public void setTargetAcceleration(double acceleration) {
-        controller.setTargetAcceleration(acceleration);
+        controller.setTargetVelocity(acceleration);
     }
 
     public void setTargetVelocity(double velocity) {
-        controller.setTargetVelocity(velocity);
         controller.setTargetPosition(velocity);
     }
 
@@ -57,33 +58,37 @@ public class VelocityPIDFController {
         if (dx != 0.0 && Math.abs(measuredVelocity - lastVelocity) > VELOCITY_EPSILON) {
             double accel = (measuredVelocity * measuredVelocity - lastVelocity * lastVelocity) / (2.0 * dx);
 
-            for (int i = 0; i < ACCEL_SAMPLES; i++) {
-                accelSamples.add(accel);
-            }
-        } else {
-            double dt = clock.seconds() - lastUpdateTimestamp;
-            double accel = (measuredVelocity - lastVelocity) / dt;
+            lastPosition = measuredPosition;
+            lastVelocity = measuredVelocity;
+
             accelSamples.add(accel);
+        } else {
+            accelSamples.add(0.0);
         }
 
         return accelSamples.getMean();
     }
 
     public double update(double measuredPosition, double measuredVelocity) {
+        if (Double.isNaN(lastPosition)) {
+            lastPosition = measuredPosition;
+        }
+        if (Double.isNaN(lastVelocity)) {
+            lastVelocity = measuredVelocity;
+        }
+
         double accel = calculateAccel(measuredPosition, measuredVelocity);
 
-        lastPosition = measuredPosition;
-        lastVelocity = measuredVelocity;
-        lastUpdateTimestamp = clock.seconds();
-
-        return controller.update(measuredVelocity, accel);
+        double correction = controller.update(measuredVelocity, accel);
+        double feedforward = Kinematics.calculateMotorFeedforward(
+                controller.getTargetPosition(), controller.getTargetVelocity(), kV, kA, kStatic);
+        return correction + feedforward;
     }
 
     public void reset() {
         controller.reset();
 
-        lastPosition = 0.0;
-        lastPosition = 0.0;
-        lastUpdateTimestamp = clock.seconds();
+        lastPosition = Double.NaN;
+        lastVelocity = Double.NaN;
     }
 }
